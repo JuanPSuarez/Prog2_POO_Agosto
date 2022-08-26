850665422ebef367259a2e085f06fd5d73daac0d
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-08-26 10:37:11 -0300

    Update Ejercicio1.py
    Juan te añadí algunas sugerencias como comentarios, tu propuesta de resolución ya cumple el objetivo del ejercicio.  Continua trabajando de esta manera, TE FELICITO !

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,15 +2,19 @@ class persona:
     def __init__(self):
 #Nombre
         self.nombre = input("Ingrese el nombre: ")
-        while len(self.nombre) < 2 or self.nombre.isalpha() != True: #Verificacion longitud y letras. Mayor a 2 letras, solo letras.
+    #(esta validacion permite un ingreso desmedido de caracteres, establece un limite que consideres oportuno para el "largo" del nombre)
+    #(no permite espacios para añadir el segundo nombre, es decir solo permite ingresar "maria" y no "maria agustina")
+        while len(self.nombre) < 2 or self.nombre.isalpha() != True: #Verificacion longitud y letras. Mayor a 2 letras, solo letras. 
             self.nombre = input("Ingrese un nombre valido: ")
 #Edad
         self.edad = input("Ingrese la edad: ")
+    #(deberiamos establecer un limite de edad maxima)
         while self.edad.isnumeric() == False: #Verificacion de si es numero.
             self.edad = input("Ingrese una edad valida: ")
         self.edad = int(self.edad) #Se asigna edad de string a numeros.
 #DNI
         self.DNI = input("Ingrese el DNI: ")
+    #(acepta 5numeros como minimo)
         while len(self.DNI) not in range (5, 9): #La longitud de DNI debe ser entre el rango de 6 y 8.
             self.DNI = (input("DNI demasiado largo o corto. Ingrese entre 6 y 8 numeros. Agrege '0' al principio si es necesario: "))
         self.DNI = int(self.DNI) #Se asigna el DNI de string a numeros.
