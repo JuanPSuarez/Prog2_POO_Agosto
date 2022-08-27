5c2733bb368ad3dc83ca73b08f0c84dd44b3045a
JuanS <juanpa_9666@hotmail.com>
2022-08-27 17:36:43 -0300

    Added DNI only numbers exception. Fixed mostrarPersona. Commented Getters & Setters.

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,7 +2,9 @@ class persona:
     def __init__(self):
 #Nombre
         self.nombre = input("Ingrese el nombre: ")
-        while len(self.nombre) < 2 or any(char.isdigit() for char in self.nombre) or len(self.nombre) > 30: #Verificacion longitud y letras. Mayor a 2 letras, solo letras 
+        
+        while len(self.nombre) < 2 or any(char.isdigit() for char in self.nombre) or len(self.nombre) > 30: 
+            #Verificacion longitud y letras. Tiene que tener minimo 2 caracteres, maximo 30. Solo letras, no numeros.
             self.nombre = input("Ingreso un nombre inválido. Ingrese el nombre nuevamente: ") 
 #Edad
         self.edad = input("Ingrese la edad: ")
@@ -10,39 +12,49 @@ class persona:
             self.edad = input("Ingreso una edad con caracteres incorrectos. Ingrese una edad con numeros unicamente: ")
         
         self.edad = int(self.edad) #Se convierte a int      
-        while self.edad < 1 or self.edad > 150:
+        while self.edad < 1 or self.edad > 150: #Se verifica si esta dentro del rango 1 a 150 años.
             self.edad = int(input("Edad fuera de rango. Ingrese una edad entre 1 a 150 años:  "))
 
 #DNI
         self.DNI = input("Ingrese el DNI: ")
-        while len(self.DNI) not in range (6, 9): #La longitud de DNI debe ser entre el rango de 6 y 8.
-            self.DNI = (input("DNI demasiado largo o corto. Ingrese entre 6 y 8 numeros. Agrege '0' al principio si es necesario: "))
+        while len(self.DNI) not in range (6, 9) or any(num.isalpha() for num in self.DNI): #La longitud de DNI debe ser entre el rango de 6 y 8.
+            self.DNI = (input("DNI Incorrecto. Ingrese solo numeros. 6 a 8 caracteres. Agregar '0' si es necesario:  "))
         self.DNI = int(self.DNI) #Se asigna el DNI de string a numeros.
 
+#MayorEdad
         self.Mayor = bool
         
 
 #getters
-    def getNombre(self):
-        return self.nombre
-    def getEdad(self):
-        return self.edad
-    def getDNI(self):
-        return self.DNI
+    # @property
+    # def nombre(self):
+    #     return self.nombre
+    # @property
+    # def edad(self):
+    #     return self.edad
+    # @property
+    # def dni(self):
+    #     return self.DNI
 
 #setters
-    # def setPersona(self):
-    #     pass
+    # @nombre.setter
+    # def Persona(self, nombre):
+    #     self.nombre(self)
+    # @edad.setter
+    # def Edad(self, edad):
+    #     self.nombre = edad
+    # @dni.setter
+    # def DNI(self, dni):
+    #     self.DNI = dni
 
 
 # Metodo mostrar()
     def mostrarPersona(self):
-        print("Los datos de la persona son:")
-        print("Nombre: ", self.nombre)
-        print("Edad: ", self.edad)
-        print("DNI: ", self.DNI)
-
-        
+        print (f"Los datos de la persona son: ", 
+        "\n Nombre:" , self.nombre,
+        "\n Edad: ",self.edad,
+        "\n DNI: ",self.DNI
+        )
 
 # Metodo esMayorDeEdad()
 
